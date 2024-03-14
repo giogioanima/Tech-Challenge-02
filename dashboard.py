b97b7f0e3c468231f83246bbd1479a6323802848
Gio Queiroz <104872161+giogioanima@users.noreply.github.com>
2024-03-14 04:36:11 -0300

    v03

--- dashboard.py
+++ dashboard.py
@@ -25,25 +25,47 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-#Importando banco de dados
-df = pd.read_csv("C:\Users\gih\Downloads\postechchall02\Dados Históricos - Ibovespa Fase 02.csv", sep=",")
-df = df.drop(columns=['Abertura', 'Máxima', 'Mínima', 'Vol.', 'Var%'])
-df.rename(columns={"Data": "Date", "Último": "Close"}, inplace=True)
+##Importando banco de dados
+#df = pd.read_csv(r"C:\Users\gih\Downloads\postechchall02\Dados Históricos - Ibovespa Fase 02.csv", sep=",")
+#df = df.drop(columns=['Abertura', 'Máxima', 'Mínima', 'Vol.', 'Var%'])    
+#df.rename(columns={'Data': "Date", 'Último': 'Close'}, inplace=True)    
 
-##Importando banco de dados com yfinance
-#import yfinance as yf
-#symbol = '^BVSP'
-#start_date = '2014-01-01'
-#end_date = '2024-03-07'
-#df = yf.download(symbol, start=start_date, end=end_date)
+#Importando banco de dados com yfinance
+import yfinance as yf
+symbol = '^BVSP'
+start_date = '2014-01-01'
+end_date = '2024-03-07'
+df = yf.download(symbol, start=start_date, end=end_date)
 
 #Conversão da data para formato datetime
 df = df.reset_index('Date')
+df = df.sort_values(by="Date", ascending=True)
+df['Date'] = pd.to_datetime(df['Date'], format="%d.%m.%Y") 
+
+
+
+# PROPHET #
+df = yf.download(symbol, start=start_date, end=end_date)
+df = df.reset_index('Date')
 df['Date'] = pd.to_datetime(df['Date'])
+df.drop(columns=['Open', 'High', 'Low', 'Volume', 'Adj Close'], inplace=True)
+df[['ds','y']] = df[['Date','Close']]
+df.head()
+
+train_data = df.sample(frac=0.8, random_state=0)
+test_data = df.drop(train_data.index)
+print(f'training data size : {train_data.shape}')
+print(f'testing data size : {test_data.shape}')
+
+modelo = Prophet(daily_seasonality=True)
+modelo.fit(train_data)
+futuro = modelo.make_future_dataframe(periods=30, freq='D')
+
+forecast = modelo.predict(futuro)
 
 
-##### INÍCIO DO DASHBOARD #####
 
+##### INÍCIO DO DASHBOARD ##### 
 st.set_page_config(page_title="Tech Challenge 02 - Gio Queiroz", page_icon=":chart_with_upwards_trend:", layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 # CABEÇALHO
@@ -161,65 +183,40 @@ with tab1:
 
     coluna4, coluna5 = st.columns(2, gap="large")
     with coluna4:
-        st.write("O gráfico de linha abaixo está sendo utilizado para exibir a flutuação diária do fechamento do IBOVESPA nos últimos 10 anos. O eixo x representa a série temporal, enquanto o eixo y indica os valores de fechamento.")    
-        option = st.selectbox(
-        'Selecione o gráfico:',
-        ('Série Temporal (2014 - 2024)', 'Volume Exportado por Ano (Litros)'))
-        if option == "Série Temporal (2014 - 2024)":
-            ax = px.line(df, x="Date", y="Close", color_discrete_sequence=['#d7e46f'], width=800, height=500, range_y=[0, 150000], title="Valores Diários de Fechamento (IBOVESPA) - Série Temporal")
-            ax.update_xaxes(dtick=1)
-            ax.update_layout(
-            yaxis=dict(
-            title="Valor de Fechamento",
-            titlefont=dict(size=16),
-            tickfont=dict(size=14),
-            showgrid=True,
-            gridcolor="#c2c0c0",
-            griddash="dash",
-            title_standoff=30 # The higher the value, the farther away it is displayed
-            ),
-            xaxis = dict(
-            title="Ano",
-            tickmode = 'array',
-            tickvals = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025],
-            titlefont=dict(size=16),
-            tickfont=dict(size=14),
-            tickangle=-45,
-            title_standoff=20 # The higher the value, the farther away it is displayed
-            ))
-            ax.add_hline(y="Close", line_color="black", line_width=1, line_dash="dot", label=dict(text="                                                                  Valor Médio: US$ 7,51 MM", textposition="middle", font=dict(size=14, color="black"))
-            )
-            st.plotly_chart(ax, use_container_width=True)
+        st.write("O gráfico de linha abaixo está sendo utilizado para exibir a flutuação diária do fechamento do IBOVESPA nos últimos 10 anos. O eixo x representa a série temporal, enquanto o eixo y indica os valores de fechamento.")
+        ax = px.line(df, x="Date", y="Close", color_discrete_sequence=['#d7e46f'], width=800, height=500, range_y=[0, 150000], title="Valores Diários de Fechamento (IBOVESPA) - Série Temporal")
+        ax.update_xaxes(dtick=1)
+        ax.update_layout(
+        yaxis=dict(
+        title="Valor de Fechamento",
+        titlefont=dict(size=16),
+        tickfont=dict(size=14),
+        showgrid=True,
+        gridcolor="#c2c0c0",
+        griddash="dash",
+        title_standoff=30 # The higher the value, the farther away it is displayed
+        ),
+        xaxis = dict(
+        title="Ano",
+        tickmode = 'array',
+        tickvals = [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025],
+        titlefont=dict(size=16),
+        tickfont=dict(size=14),
+        tickangle=-45,
+        title_standoff=20 # The higher the value, the farther away it is displayed
+        ))
+        ax.add_hline(y="Close", line_color="black", line_width=1, line_dash="dot", label=dict(text="                                                                  Valor Médio: US$ 7,51 MM", textposition="middle", font=dict(size=14, color="black"))
+        )
+        st.plotly_chart(ax, use_container_width=True)
 
-        elif option == "Volume Exportado por Ano (Litros)":
-            bx = px.bar(df, x="ano", y="quantidade", color_discrete_sequence=['#F2CDA0'], width=800, height=500, range_x=[2007, 2023], range_y=[0, 25_000_000], title="Volume Exportado por Ano (Litros)")
-            bx.update_xaxes(dtick=1)
-            bx.update_layout(
-            yaxis=dict(
-            title="Valor Exportado (US$)",
-            titlefont=dict(size=16),
-            tickfont=dict(size=14),
-            showgrid=True,
-            gridcolor="#c2c0c0",
-            griddash="dash",
-            title_standoff=20 # The higher the value, the farther away it is displayed
-            ),
-            xaxis = dict(
-            title="Ano",
-            tickmode = 'array',
-            tickvals = [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022],
-            titlefont=dict(size=16),
-            tickfont=dict(size=14),
-            tickangle=-45,
-            title_standoff=20 # The higher the value, the farther away it is displayed
-            ))
-            bx.add_hline(y=media_quantidade, line_color="black", line_width=1, line_dash="dot", label=dict(text="                                                           Volume Médio: 5,87 MM litros", textposition="middle", font=dict(size=14, color="black"))
-            )
-            st.plotly_chart(bx, use_container_width=True)
-
-    ####
-plt.figure(figsize = (15,10))
-plt.plot(df['Date'], df['Close'], label='IBOVESPA')
-
-plt.legend(loc='best')
-plt.show()
+    with coluna5:        
+        st.markdown(':triangular_flag_on_post: <h class="about3-style">2014 - 2016</h>', unsafe_allow_html=True)
+        st.write("- As exportações de vinho brasileiro enfrentaram o câmbio desfavorável e a crise econômica internacional iniciada em 2008. Os preços no mercado internacional caíram, tornando a competitividade da uva brasileira mais desafiadora. Além disso, novas ameaças surgiram, como a produção de uvas sem sementes em escala no Peru, onde os custos são menores.")
+        st.write(" ")
+        st.markdown(':chart: <h class="about3-style">2016 - 2019</h>', unsafe_allow_html=True)
+        st.write("- Em 2013, as exportações de vinho tiveram um pico significativo devido ao aumento expressivo no volume exportado, impulsionado pela exportação do vinho a granel, tanto de mesa quanto fino, aproveitando o programa de escoamento de produção (PEP), que conferia vantagem competitiva no mercado internacional. No entanto, houve uma desvalorização no valor do produto nesse ano. ")
+        st.write(" ")
+        st.markdown(':chart_with_downwards_trend: <h class="about3-style">2020 - atualmente</h>', unsafe_allow_html=True)
+        st.write("- Em contraste, em 2015, as exportações caíram drasticamente pela falta de adoção do PEP, resultando em uma diminuição acentuada no volume exportado. A valorização do produto não compensou a ausência do programa, enquanto a crise política e os escândalos envolvendo a Petrobrás que vieram à tona também contribuíram para o cenário desfavorável.")
+        st.write(" ")
+        st.markdown('<h class="about3-style">Fontes: <h n class="about2-style"><a href="https://www.infoteca.cnptia.embrapa.br/bitstream/doc/922116/1/PROTASpanoramavitivinicultura2010.pdf">1</a> | <a href="https://releia.ifsertao-pe.edu.br/jspui/bitstream/123456789/821/1/TCC%20-%20PANORAMA%20DA%20EXPORTAÇÃO%20E%20IMPORTAÇÃO%20DE%20VINHOS%20NO%20BRASIL.pdf">2</a>  | <a href="https://www.embrapa.br/busca-de-noticias/-/noticia/9952204/artigo-desempenho-da-vitivinicultura-brasileira-em-2015">3</a>  | <a href="https://www.poder360.com.br/brasil/venda-de-vinho-no-brasil-aumentou-37-na-pandemia/">4</a></h></h>', unsafe_allow_html=True)
